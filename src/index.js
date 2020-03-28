b2db1539817b9d0ff0642ddb3ea0580f775ab566
Jacob McCrumb <jsmccrumb@gmail.com>
2020-03-28 11:06:30 -0400

    Move original and reinit react

--- src/index.js
+++ src/index.js
@@ -1,40 +1,17 @@
-/* eslint-disable import/default */
-
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-import ReactGA from 'react-ga';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import routes from './routes';
-import configureStore from './store/configureStore';
-require('./favicon.ico');
-import './assets/styles/app.scss';
-import './assets/styles/bootstrap-superhero.min.css';
-import './assets/styles/animate.min.css';
-import './assets/styles/font-awesome.min.css';
-import './assets/styles/styles.scss';
-
-import 'jquery';
-import 'web-animations-js/web-animations.min';
-
-
-const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
-
-function logPageView() {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
-}
-
-ReactGA.initialize('UA-82047208-2');
-
-render(
-  <Provider store={store}>
-    <Router
-      history={history}
-      routes={routes}
-      onUpdate={logPageView} />
-  </Provider>, document.getElementById('app')
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
